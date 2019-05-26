3aa6d9b098ac651e69a338a79649b839c7ce7128
Vanina <vanina.tobar@gmail.com>
2019-05-26 16:41:03 -0400

    configuracion 1

---
+++ helloselenium/src/main/java/selenium/helloselenium/App.java
@@ -0,0 +1,13 @@
+package selenium.helloselenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
